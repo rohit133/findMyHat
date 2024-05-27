005c67502392d440d120f39c3d291431d7d7c8bc
rohit133 <rohityou000@gmail.com>
2024-05-27 13:27:17 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+class Field {
+  constructor(field){
+    this.field = field
+  }
+
+}
+
+
+
+
+// Testing code. 
+
+// const myField = new Field([
+//   ['*', '░', 'O'],
+//   ['░', 'O', '░'],
+//   ['░', '^', '░'],
+// ]);
+
+
+
+ 
