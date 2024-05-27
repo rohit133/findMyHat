17446b28435325e27bddbae1942a0cc50a697832
rohit133 <rohityou000@gmail.com>
2024-05-27 18:17:50 +0530

    Completed code

--- main.js
+++ main.js
@@ -5,69 +5,57 @@ const hole = "O";
 const fieldCharacter = "░";
 const pathCharacter = "*";
 
-let currentlyPlaying = true;
-
 class Field {
   constructor(field) {
     this._field = field;
     this.x = 0;
     this.y = 0;
+    this.currentlyPlaying = true;
   }
+
   print() {
     for (let row of this._field) {
       console.log(row.join(" "));
     }
   }
-  moves() {
-    // use a loop to run the program until it the user wins or lose!
-    let move = prompt("Which way you would like to move?");
+
+  play() {
+    this.print();
+    let move = prompt("Which way you would like to move? ");
     switch (move.toLowerCase()) {
       case "u":
-        console.log("Moving Up");
         this.y -= 1;
         break;
       case "d":
-        console.log("Moving Down");
         this.y += 1;
         break;
       case "l":
-        console.log("Moving Left");
         this.x -= 1;
         break;
       case "r":
-        console.log("Moving Right");
         this.x += 1;
         break;
       default:
         console.log(
           "Please choose any of these Options: {r}: Right {l}: Left {u}: Up {d}: Down "
         );
-        break;
+        return;
+    }
+
+    if (this.y < 0 || this.y >= this._field.length || this.x < 0 || this.x >= this._field[0].length) {
+      console.log("You lose - Out of boundary");
+      this.currentlyPlaying = false;
+    } else if (this._field[this.y][this.x] === hole) {
+      console.log("You lose - Fell down in a hole!");
+      this.currentlyPlaying = false;
+    } else if (this._field[this.y][this.x] === hat) {
+      console.log("You Win - You found the Hat");
+      this.currentlyPlaying = false;
+    } else {
+      this._field[this.y][this.x] = pathCharacter;
     }
   }
-  checkWin() {
-    switch (this._field[this.x][this.y]) {
-      case undefined:
-        console.log("You lose - Out of boundary");
-        currentlyPlaying = false;
-        break;
-      case hole:
-        console.log("You lose - Fell down in a hole!");
-        currentlyPlaying = false;
-        break;
-      case hat:
-        console.log("You Win - You found the Hat");
-        currentlyPlaying = false;
-        break;
-      case fieldCharacter:
-        console.log("Keep looking for the hat...");
-        this._field[this.y][this.x] = pathCharacter;
-        break;
-      case pathCharacter:
-        console.log("You are stepping on *");
-        break;
-    }
-  }
+
   static generateField(height, width, holes = 5) {
     let fieldArray = [];
     for (let i = 0; i < height; i++) {
@@ -76,31 +64,40 @@ class Field {
         fieldArray[i].push(fieldCharacter);
       }
     }
-    for (let k = holes; k > 0; k--) {
-      fieldArray[Math.floor(Math.random() * height)][
-        Math.floor(Math.random() * width)
-      ] = hole;
+
+    while (holes > 0) {
+      let holeY = Math.floor(Math.random() * height);
+      let holeX = Math.floor(Math.random() * width);
+      if (fieldArray[holeY][holeX] === fieldCharacter) {
+        fieldArray[holeY][holeX] = hole;
+        holes--;
+      }
     }
-    fieldArray[Math.floor(Math.random() * height)][
-      Math.floor(Math.random() * width)
-    ] = hat;
+
+    let hatY, hatX;
+    do {
+      hatY = Math.floor(Math.random() * height);
+      hatX = Math.floor(Math.random() * width);
+    } while (fieldArray[hatY][hatX] !== fieldCharacter);
+
+    fieldArray[hatY][hatX] = hat;
     fieldArray[0][0] = pathCharacter;
     return fieldArray;
   }
 }
 
 // Testing code.
-let myField;
-let gameMaze = Field.generateField(4, 4 );
-myField = new Field(gameMaze);
+let counter = 0;
+console.log(`First count ${counter}`);
+const gameMaze = Field.generateField(4, 4);
+const myField = new Field(gameMaze);
 
-function play() {
-  while (currentlyPlaying) {
-    myField.print()
-    myField.moves();
-    myField.checkWin();
+function start() {
+  counter += 1;
+  while (myField.currentlyPlaying) {
+    myField.play();
   }
   console.log("Game Over!");
 }
 
-play();
+start();
